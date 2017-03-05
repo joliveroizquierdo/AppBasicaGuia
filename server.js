fb74e774889a6f7bda5399a2ac7ef84b231b3322
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 15:19:27 -0500

    cross 2

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const app  = express()
 	  app.use(bodyParser.urlencoded({extended: true}))
 
 	  //app.use(cors())
-	  app.use(function(req, res, next) {
-		  res.header("Access-Control-Allow-Origin", "*");
-		  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  app.all('*', function(req, res, next) {
+		  res.header('Access-Control-Allow-Origin', '*');
+		  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+		  res.header('Access-Control-Allow-Headers', 'Content-Type');
 		  next();
-	  })
+	  });
 
 	  app.use('/api', routes)
 
