1edbdc59fad9bd427b4ea4fea8aa80feccc1e0a4
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 15:28:34 -0500

    cros

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ const app  = express()
 
 	  app.use(bodyParser.urlencoded({extended: true}))
 
-	  //app.use(cors())
-	  app.use(function(req, res, next) {
-		  res.header('Access-Control-Allow-Origin', '*');
-		  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-		  res.header('Access-Control-Allow-Headers', 'Content-Type');
-		  next();
-	  });
+	  app.use(cors())
 
 	  app.use('/api', routes)
 
