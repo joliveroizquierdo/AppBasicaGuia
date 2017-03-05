0f4309891f1f0a740d6dabb085a116ccb66c8582
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 09:53:03 -0500

    Ruta simple, que solo retorna un mensaje

--- server.js
+++ server.js
@@ -10,16 +10,13 @@ const app  = express()
 
 	  app.use(bodyParser.urlencoded({extended: true}))
 
-	  //app.use(cors())
+	  app.use(cors())
 
-	  app.use(cors({origin: 'http://localhost:8080'}));
+	  //app.use(cors({origin: 'http://localhost:8080'}));
 
-	  /*app.use(function(req, res, next) {
-		  res.header("Access-Control-Allow-Origin", "*");
-		  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-		  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		  next()
-	  })*/
+	  app.get('/', function (req, res, next) {
+		  res.json({msg: 'This is CORS-enabled for all origins!'})
+	  })
 
 	  app.use('/api', routes)
 
