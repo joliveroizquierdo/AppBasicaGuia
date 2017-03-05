97d0a3584c0527562d3ac84c4c5760e6f74ee6f4
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 09:48:23 -0500

    Especificacndo la ruta de mi maquina

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const app  = express()
 
 	  app.use(bodyParser.urlencoded({extended: true}))
 
-	  app.use(cors())
+	  //app.use(cors())
 
-	  app.use(function(req, res, next) {
+	  app.use(cors({origin: 'http://localhost:8080'}));
+
+	  /*app.use(function(req, res, next) {
 		  res.header("Access-Control-Allow-Origin", "*");
 		  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 		  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		  next()
-	  })
+	  })*/
 
 	  app.use('/api', routes)
 
