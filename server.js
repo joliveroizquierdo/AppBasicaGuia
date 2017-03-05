59b12b6afa4df773072802668d6be1c2fe84d748
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 09:41:27 -0500

    permisos de acceso diferentes origines

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const app  = express()
 
 	  app.use(cors())
 
+	  app.use(function(req, res, next) {
+		  res.header("Access-Control-Allow-Origin", "*");
+		  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		  next()
+	  })
+
 	  app.use('/api', routes)
 
 	  app.listen(port, () => console.log('El servidor está corriendo en el puerto: ' + port))
