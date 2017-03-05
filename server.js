0e407bf41d445f7eed9b82e87b1c0644832c300f
Jorge Armando Olivero <joliveroizquierdo@gmail.com>
2017-03-05 09:13:23 -0500

    USando cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const   express    		= require('express')
 const	bodyParser 		= require('body-parser')
 const	routes 			= require('./routes/rutas')
+const   cors            = require('cors')
 
 const port = 8080;
 const app  = express()
@@ -8,6 +9,8 @@ const app  = express()
 	  app.use(bodyParser.json())
 
 	  app.use(bodyParser.urlencoded({extended: true}))
+
+	  app.use(cors())
 
 	  app.use('/api', routes)
 
